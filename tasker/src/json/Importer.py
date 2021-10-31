b10ff2e411182fa0bb39867ecfa9bcd590d892f3
StaGlo <stanley.glowczewski@gmail.com>
2021-10-31 18:30:06 +0100

    added tasker files

---
+++ tasker/src/json/Importer.py
@@ -0,0 +1,24 @@
+import json
+
+
+class Importer:
+    imported_data = []
+
+    def __init__(self):
+        pass
+
+    def read_tasks(self):
+        # TODO odczytaj plik i zdekoduj treść tutaj
+
+        with open("taski.json") as file:
+            self.imported_data = json.load(file)
+            file.close()
+
+        pass
+
+    def get_tasks(self):
+        # TODO zwróć zdekodowane taski tutaj
+
+        return self.imported_data
+
+        pass
